1c16d348ff39f469dd5f2b5a2723efb1360f47ed
Keenan Brock <keenan.brock@ping4.com>
2013-03-13 21:03:25 -0400

    no longer calling gets returning a single error (since that is all that apple will send) fix notifications bug

--- lib/grocer/pusher.rb
+++ lib/grocer/pusher.rb
@@ -15,23 +15,19 @@ module Grocer
 
     def push_and_check_batch(notifications, final_timeout = 1.0)
       errors = []
-      notifications.each_with_index { |n, i| n[:identifier] = i }
+      notifications.each_with_index { |n, i| n.identifier = i }
 
       i = 0
       while (i < notifications.length)
         notification = notifications[i]
 
         #block on the last message out by sending a timeout
-        timeout = (i == notifications.length) ? final_timeout : nil
+        timeout = (i == notifications.length - 1) ? final_timeout : nil
 
         if error = push_and_check(notification, timeout)
           i = notifications.index { |n| n.identifier == error[:identifier] }
           error[:notification] = notifications[i]
           errors << error
-
-          #if this is not single message error
-          #  return errors
-          #end
         end
         i += 1
       end
@@ -41,30 +37,14 @@ module Grocer
     # pass no timeout to not block (typical use case)
     # pass a timeout to block for a certain amount of time (use when you are done sending)
     def read_errors(timeout = nil)
-      errors = []
-      read, write, err = select(timeout)
-      if err && err.first
-        errors << {identifier: 'unexpected error while reading errors'}
-      end
+      read, write, err = @connection.select(timeout)
       if read && read.first
-        while error_hash = read_error
-          errors << error_hash
+        if error = @connection.read(6)
+          close
+          e = error.unpack("c1c1N1")
+          return { identifier: e[2], error_code: e[1] }
         end
       end
-
-      close if errors.count > 0 
-      errors
-    end
-
-    def read_error
-      if error = @connection.gets
-        e = error.unpack("c1c1N1")
-        { identifier: e[2], error_code: e[1] }
-      end
-    end
-
-    def select(timeout)
-      @connection.select(timeout)
     end
 
     def connect
--- spec/grocer/pusher_spec.rb
+++ spec/grocer/pusher_spec.rb
@@ -16,11 +16,6 @@ describe Grocer::Pusher do
   end
 
   context 'delegation' do
-    it "#select" do
-      subject.select(55)
-      connection.should have_received(:select).with(55)
-    end
-
     it "#close" do
       subject.close
       connection.should have_received(:close)
@@ -34,19 +29,10 @@ describe Grocer::Pusher do
 
   context "with error" do
     it "#read_errors" do
-      connection.expects(:gets).returns([8, 6, 105].pack("ccN"))
-      subject.read_error.should == {identifier: 105, error_code: 6}
-    end
-
-    it "#read_errors" do
       connection.expects(:select).returns([[connection],[connection]])
-      # return 1 error, then nil
-      connection.expects(:gets).twice.returns(
-        [8, 6, 105].pack("ccN"),
-        nil
-      )
+      connection.expects(:read).returns([8, 6, 105].pack("ccN"))
       connection.expects(:close)
-      subject.read_errors.should == [{identifier: 105, error_code: 6}]
+      subject.read_errors.should == {identifier: 105, error_code: 6}
     end
   end
 
@@ -60,23 +46,21 @@ describe Grocer::Pusher do
   it "#resend push for token errors" do
     # send 1 , 2 , 3, 4, (error on 2), 5 ; resend 3, 4
 
-    notifications = 5.times.map { |i| stub(identifier: i, '[]=' => nil)}
+    notifications = 5.times.map { |i| stub(identifier: i, 'identifier=' => nil, 'alert' => 'alert')}
     bad_notification = notifications[1]
 
-    subject.stubs(:push) #.exactly(7).times
-
-    subject.stubs(:read_errors).returns(
+    subject.expects(:read_errors).with(nil).at_least_once.returns(
       nil, nil, nil,
       #after sending 4th, return error on 2nd
       { identifier: bad_notification.identifier, error_code: 8 },
       nil, nil, nil, nil
     )
+    subject.expects(:read_errors).with(1.0).returns(nil)
     subject.expects(:push).with(notifications[0]).once
     subject.expects(:push).with(notifications[1]).once
     subject.expects(:push).with(notifications[2]).twice
     subject.expects(:push).with(notifications[3]).twice
     subject.expects(:push).with(notifications[4]).once
-
     subject.push_and_check_batch(notifications)
   end
 end
