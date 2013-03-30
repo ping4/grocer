cfd50b33197ba344f7d9b1e1720d18abc4c19d64
Keenan Brock <keenan@thebrocks.net>
2013-03-29 22:50:45 -0400

    remove stub_everything from pusher_spec

--- spec/grocer/pusher_spec.rb
+++ spec/grocer/pusher_spec.rb
@@ -2,16 +2,16 @@ require 'spec_helper'
 require 'grocer/pusher'
 
 describe Grocer::Pusher do
-  let(:connection) { stub_everything }
+  let(:connection) { stub('Connection') }
 
   subject { described_class.new(connection) }
 
   describe '#push' do
     it 'serializes a notification and sends it via the connection' do
-      notification = stub(:to_bytes => 'abc123')
+      connection.expects(:write).with('abc123')
+      notification = stub(to_bytes: 'abc123', write: nil)
       subject.push(notification)
 
-      connection.should have_received(:write).with('abc123')
     end
   end
 end
